52059bb4d12b35ef409243094a6bd6788fef0fe0
thiyangt <thiyanga.talagala@monash.edu>
2025-04-22 11:29:10 +0530

    updated

---
+++ R/shark_ray.R
@@ -0,0 +1,28 @@
+#' Phylogenetic data files recording body mass, brain volume and brain mass: shark ray
+#'
+#'@description Phylogenetic data files recording body mass, brain volume and brain mass of shark ray by Tsuboi, M., van der Bijl, W., Kopperud, B.T., Erritzøe, J., Voje, K.L., Kotrschal, A., Yopak, K.E., Collin, S.P., Iwaniuk, A.N. and Kolm, N., 2018. Breakdown of brain–body allometry and the encephalization of birds and mammals. Nature Ecology & Evolution, 2(9), pp.1492-1500.
+#'@format A data frame with 450 rows and 18 variables
+#'\description{
+#'\item{Order}{Order}
+#'\item{Family}{Family}
+#'\item{Genus}{Genus}
+#'\item{Species}{Species}
+#'\item{Subspecies}{Subspecies}
+#'\item{Genus_Species}{Genus species}
+#'\item{Sample size}{The sample size used to estimate static allometries}
+#'\item{Sex}{Gender: m-male, f-female}
+#'\item{Method}{Method of measuring brain size}
+#'\item{Body mass (g)}{Body mass (g)}
+#'\item{Body mass S.D}{Body mass S.D}
+#'\item{Brain mass (g)}{Brain mass (g)}
+#'\item{Brain mass S.D}{Brain mass S.D}
+#'\item{Age Class}{Age Class}
+#'\item{Locality}{Locality}
+#'\item{Species Name in original article}{Species Name in original article}
+#'\item{Reference}{Reference}
+#'\item{Remark}{Remark}
+#'}
+#'@source Tsuboi, M., van der Bijl, W., Kopperud, B.T., Erritzøe, J., Voje, K.L., Kotrschal, A., Yopak, K.E., Collin, S.P., Iwaniuk, A.N. and Kolm, N., 2018. Breakdown of brain–body allometry and the encephalization of birds and mammals. Nature Ecology & Evolution, 2(9), pp.1492-1500.
+#'@examples
+#'data(shark_ray)
+"shark_ray"
